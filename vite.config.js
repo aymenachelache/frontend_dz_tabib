f64a16d7ca7d5519735c71c1abd6a738f78584c5
aymenachelache <achelache.aymen@gmail.com>
2025-01-25 22:41:53 +0100

    last

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'; // Import the path module
+import { fileURLToPath } from 'url';
+
+// Get __dirname equivalent in ES modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    extensions: ['.js', '.jsx', '.css', '.scss'], // Add extensions to resolve
+    alias: {
+      '@styles': path.resolve(__dirname, 'src/styles'), // Example alias
+      '@components': path.resolve(__dirname, 'src/components'), // Example alias
+    },
+  },
+
   server: {
     port: 5173,
   },
